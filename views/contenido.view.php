5029ff3e973d71fa39a8406e07dc88c701188872
Martin <44913087+IngCampos@users.noreply.github.com>
2019-10-21 21:14:51 -0500

    Despliegue de materias que da el maestro
    Se muestra en una tabla las materias que da el maestro ordenadas por la hora, junto con informacion basica(toda menos descripcion).
    Esta informacion solo se muestra si es maestro el usuario

--- views/contenido.view.php
+++ views/contenido.view.php
@@ -11,8 +11,28 @@ require 'header.php';
 	codigo en caso de ser alumno
 		<?php endif ?>
 		<?php if ($_SESSION['usuario']['Titulo']=="Maestro"): ?>
-	codigo en caso de ser maestro
-		<?php endif ?>
+		<center><h2>Grupos asignados</h2></center>
+		<?php 
+		try {
+			$conexion = new PDO('mysql:host=localhost;dbname=escuela_bd', 'root', '');
+		} catch (PDOException $e) {
+			echo "Error:" . $e->getMessage();
+		}
+		$statement = $conexion->prepare('SELECT materias.Nombre, materias.Grado, materias.Descripcion, clases.Hora FROM clases INNER JOIN materias ON clases.Id_materia=materias.Id WHERE clases.Id_maestro = :Id ORDER BY clases.Hora');
+		$statement->execute(array(
+			':Id' => $_SESSION['usuario']['0']
+			//se pone 0 por que al haber dos campos con el nombre Id, el otro pasa a nombrarse 0
+		));
+		$materias = $statement->fetch();
+		echo "<table ><tr><td>Hora</td><td>Materia</td><td>Grado</td><td>Asistencias</td><td>Tareas</td></tr>";
+		while($materias!=null){
+			echo "<tr><td>".$materias["Hora"]."</td><td>".$materias["Nombre"]."</td><td>".$materias["Grado"]."</td><td><a>Asistencias</a></td><td><a>Tareas</a></td></tr>";
+			$materias = $statement->fetch();
+		}
+		echo "</table>";
+		// for ($i= 0;$i<=$materias.length;)
+		// echo $materias;
+		endif ?>
 		<?php if ($_SESSION['usuario']['Titulo']=="Administrador"): ?>
 	codigo en caso de ser administrador
 		<?php endif ?>
