d91367186e1d2c08350fd5ceb5ccacd4fc7b05e6
IngCampos <15151130@mail.ita.mx>
2019-12-01 14:06:02 -0600

    Update terminosycondiciones.view.php

--- terminosycondiciones.view.php
+++ terminosycondiciones.view.php
@@ -1,10 +1,8 @@
-<link rel="stylesheet" href="librerias/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="librerias/bootstrap.min.css">
 <?php
 require 'views/header.php';
 ?>
-<script src="librerias/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="librerias/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+<script src="librerias/bootstrap.min.js"></script>
 <body>
 	<div class="contenedor">
 		<h3>Terminos y condiciones</h3>
